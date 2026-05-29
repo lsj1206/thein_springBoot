b562f29ed5e929a41334818687cb2a4580b5ebea
LSJ <1007ckddjs12@gmail.com>
2026-05-29 17:07:07 +0900

    feat: add old.file

---
+++ j0527/Myjava/src/com/lsj/Ex3.java
@@ -0,0 +1,10 @@
+package com.lsj;
+
+public class Ex3 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ j0528/j0528/src/j0528/w3c/MyPoint.java
@@ -0,0 +1,9 @@
+package j0528.w3c;
+
+public class MyPoint {
+	int x = 100;
+	
+	public void myStr() {
+		System.out.println("MyPoint 입니다.");
+	}
+}

fe0c033e6101aa08d41411d65f9773b534bfc73c
LSJ <1007ckddjs12@gmail.com>
2026-06-02 11:05:41 +0900

    feat: add 0602 project

---
+++ ex0602/src/com/lsj/MyClass.java
@@ -0,0 +1,15 @@
+package com.lsj;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		Car1 car1 = new Car1("Corvette");
+		
+		Car1 car2 = new Car1(1969, "Mustang");
+		
+		car1.printInfo();
+		car2.printInfo();
+	}
+
+}

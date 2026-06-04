b715b24859be681b1ce237ab92624e7870a7559a
LSJ <1007ckddjs12@gmail.com>
2026-06-04 15:23:02 +0900

    feat: add showinfo() func

--- ex0604/src/com/lsj_2/Animal.java
+++ ex0604/src/com/lsj_2/Animal.java
@@ -7,4 +7,16 @@ abstract class Animal {
 	protected String animalSound = "울음소리";
 	protected abstract void animalEat();
 	protected abstract void animalSleep();
+	
+	protected void showInfo() {
+		System.out.println("=========================");
+		System.out.println("이름: " + this.animalName);
+		System.out.println("품종: " + this.animalType);
+		System.out.println("소리: " + this.animalSound);
+		System.out.print("식성: ");
+		this.animalEat();
+		System.out.print("수면: ");
+		this.animalSleep();
+		System.out.println("=========================");
+	}
 }
--- ex0604/src/com/lsj_2/MyMain2.java
+++ ex0604/src/com/lsj_2/MyMain2.java
@@ -6,15 +6,19 @@ public class MyMain2 {
 		
 		Pig pig1 = new Pig();
 		System.out.println(pig1.animalName);
-		
+		System.out.println("=========================");
 		Cat cat1 = new Cat();
 		System.out.println(cat1.animalName);
-		
+		System.out.println(cat1.animalType);
+		System.out.println(cat1.animalSound);
+		System.out.println("=========================");
 		Dog dog1 = new Dog();
 		System.out.println(dog1.animalName);
-				
+		dog1.animalEat();
+		dog1.animalSleep();
+		System.out.println("=========================");
 		Bird bird1 = new Bird();
 		System.out.println(bird1.animalName);
+		bird1.showInfo();
 	}
-
 }

1ee8a6e4ebd7934ad949f6a340281f5507ce1c4a
LSJ <1007ckddjs12@gmail.com>
2026-06-23 17:51:19 +0900

    refactor: rename folder

--- 04.jsp/golf/src/main/java/golf/DBconnection.java
+++ 04.jsp/golf/src/main/java/golf/DBconnection.java
@@ -8,6 +8,14 @@ public class DBconnection {
 	private static final String URL = "jdbc:postgresql://localhost:5432/hrd";
     private static final String USER = "postgres";
     private static final String PASSWORD = "1234";
+    
+    static {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);

ac5b32fe7f6320b3226528fd0672a970ebb021e1
LSJ <1007ckddjs12@gmail.com>
2026-06-23 16:40:00 +0900

    feat: add servlet

--- 04.jsp/Ex_golf/src/main/java/golf/GolfServlet.java
+++ 04.jsp/Ex_golf/src/main/java/golf/GolfServlet.java
@@ -1,9 +1,53 @@
 package golf;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/golf")
 public class GolfServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		if (action == null) {
+			action = "home";
+		}
+
+		String path = "/index.jsp";
+
+		switch (action) {
+			case "teacher":
+				path = "/views/teacher.jsp";
+				break;
+			case "insert":
+				path = "/views/insert.jsp";
+				break;
+			case "member":
+				path = "/views/member.jsp";
+				break;
+			case "result":
+				path = "/views/result.jsp";
+				break;
+			case "home":
+			default:
+				path = "/index.jsp";
+				break;
+		}
+
+		req.getRequestDispatcher(path).forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		doGet(req, resp);
+	}
 }
--- 04.jsp/Ex_golf/src/main/webapp/layout/header.jsp
+++ 04.jsp/Ex_golf/src/main/webapp/layout/header.jsp
@@ -9,11 +9,11 @@
 <header>골프연습장 회원관리 프로그램 ver 1.0</header>
 <nav>
 <ul>
-	<li><a href="../views/teacher.jsp">강사조회</a></li>
-	<li><a href="../views/insert.jsp">수강신청</a></li>
-	<li><a href="../views/member.jsp">회원정보조회</a></li>
-	<li><a href="../views/result.jsp">강사매출현황</a></li>
-	<li><a href="../index.jsp">홈으로</a></li>
+	<li><a href="${pageContext.request.contextPath}/golf?action=teacher">강사조회</a></li>
+	<li><a href="${pageContext.request.contextPath}/golf?action=insert">수강신청</a></li>
+	<li><a href="${pageContext.request.contextPath}/golf?action=member">회원정보조회</a></li>
+	<li><a href="${pageContext.request.contextPath}/golf?action=result">강사매출현황</a></li>
+	<li><a href="${pageContext.request.contextPath}/index.jsp">홈으로</a></li>
 </ul>
 </nav>
 </body>
